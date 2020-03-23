341b43e4210108fe388ec5035ee21dc4c15819e9
vonchange <80767699@yonghui.cn>
2020-03-23 16:43:46 +0800

    日志优化

--- src/main/java/com/vonchange/jdbc/abstractjdbc/util/tpl/impl/MybatisTplImpl.java
+++ src/main/java/com/vonchange/jdbc/abstractjdbc/util/tpl/impl/MybatisTplImpl.java
@@ -5,8 +5,6 @@ import com.vonchange.jdbc.abstractjdbc.model.SqlParmeter;
 import com.vonchange.jdbc.abstractjdbc.util.tpl.Tpl;
 import com.vonchange.mybatis.tpl.MybatisTpl;
 import com.vonchange.mybatis.tpl.model.SqlWithParam;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 
@@ -17,13 +15,13 @@ import java.util.Map;
  * @date 2017/12/12.
  */
 public class MybatisTplImpl implements Tpl {
-    Logger logger = LoggerFactory.getLogger(MybatisTplImpl.class);
+    //Logger logger = LoggerFactory.getLogger(MybatisTplImpl.class);
     @Override
     public SqlParmeter generate(Map<String, Object> data, String tplStr) {
         // 扩展 insert update
         SqlParmeter sqlParmeter= new SqlParmeter();
         SqlWithParam sqlWithParam= MybatisTpl.generate(tplStr,data);
-        logger.debug("use mybatis sql generate::: {}", sqlWithParam.getSql());
+        //logger.debug("use mybatis sql generate::: {}", sqlWithParam.getSql());
         sqlParmeter.setSql(sqlWithParam.getSql());
         sqlParmeter.setParameters(sqlWithParam.getParams());
         sqlParmeter.setPropertyNames(sqlWithParam.getPropertyNames());
